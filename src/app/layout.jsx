8c7b9ad7db8e70cb169bae79e1549b2af6e31ba8
Y0nnS <phionutsss@proton.me>
2025-07-09 10:09:31 +0700

    update layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="text-blue">
+      <body className="text-blue bg-blue">
         {children}
       </body>
     </html>
